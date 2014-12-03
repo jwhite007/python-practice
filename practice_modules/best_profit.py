db23a59df05b15239601c715c4fb00b2f714c144
jwhite007 <jwhite007@comcast.net>
2014-12-02 17:06:46 -0800

    made best_profit algorithm much more efficient and to work in complete cases

--- practice_modules/best_profit.py
+++ practice_modules/best_profit.py
@@ -1,28 +1,24 @@
 #! /usr/bin/env python
 
 def best_profit(alist):
+    """
+    This function takes a list of stock prices. The indexes are 'times'.
+    The function calculates the max profit which could have be acheived by
+    buying at at a specific time and selling at a specific time. Buy time,
+    buy price, sell time, sell price, and max profit are returned.
+    """
+
     max_profit = 0
     buy_time = 0
-    sell_time = 0
-    for i in range(len(alist)):
-        for j in range(i, len(alist)):
-            if alist[j] - alist[i] > max_profit:
-                max_profit, buy_time, sell_time = alist[j] - alist[i], i, j
+    buy_time2 = 0
 
-    return buy_time, sell_time, max_profit
-
-def best_profit2(alist):
-    max_profit = 0
-    buy_time = 0
-    buy_price = 0
-    sell_time = 0
-    sell_price = 0
     for i in range(len(alist)):
         if alist[i] < alist[buy_time]:
-            buy_time = i
-            buy_price = alist[i]
+            buy_time2 = i
         else:
-            if alist[i] - alist[buy_time] > max_profit:
+            if alist[i] - alist[buy_time2] > max_profit:
+                buy_time = buy_time2
+                buy_price = alist[buy_time2]
                 (max_profit,
                  sell_time,
                  sell_price) = (alist[i] - alist[buy_time],
@@ -34,7 +30,8 @@ def best_profit2(alist):
 if __name__ == '__main__':
     from timeit import timeit
     ALIST = [8, 13, 15, 10, 6, 7, 2, 11]
+    # ALIST = [8, 13, 15, 10, 6, 7, 2, 11, 15]
+    # ALIST = [8, 13, 15, 10, 6, 7, 2]
+
     print best_profit(ALIST)
-    print best_profit2(ALIST)
     # print timeit(stmt='best_profit(ALIST)', setup='from __main__ import best_profit, ALIST')
-    # print timeit(stmt='best_profit2(ALIST)', setup='from __main__ import best_profit2, ALIST')
