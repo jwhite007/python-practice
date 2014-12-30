6f29d6a860712d781b9d3667bd6f39b9172b700b
jwhite007 <jwhite007@comcast.net>
2014-12-30 13:51:04 -0800

    minor changes to monkies.py

--- practice_modules/monkies.py
+++ practice_modules/monkies.py
@@ -14,10 +14,18 @@ def score(goal, test_string):
     for i in range(len(goal)):
         if goal[i] == test_string[i]:
             numSame = numSame + 1
-        return numSame / len(goal)
+    return numSame / len(goal)
 # print(score('methinks it is like a weasel', generate_one(28)))
 
-result = 0
-while result < 0.1:
-    result = score('methinks it is like a weasel', generate_one(28))
-print(result)
+if __name__ == '__main__':
+    from timeit import timeit
+
+    def test_func(n):
+        result = 0
+        while result < n:
+            result = score('methinks it is like a weasel', generate_one(28))
+        return result
+
+    print(timeit(stmt = 'test_func(0.1)', setup = 'from __main__ import test_func', number = 1))
+    print(timeit(stmt = 'test_func(0.2)', setup = 'from __main__ import test_func', number = 1))
+    print(timeit(stmt = 'test_func(0.3)', setup = 'from __main__ import test_func', number = 1))
